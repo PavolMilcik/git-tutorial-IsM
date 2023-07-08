dabda3a475ed0c1802e0676f52e67669ab7f0596
PavolMilcik <pavolmilcik@gmail.com>
2023-07-08 12:57:12 +0200

    Add factorial function to main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from utils.math import sum_number
+from utils.counts import factorial
+
 
 if __name__ == "__main__":
     print("Hello World")
     
     print(sum_number(3, 4))
     
+    print(factorial(5))
