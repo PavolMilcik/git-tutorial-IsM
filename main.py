c625d7a3974a3950b3312b8acd6df49e13321615
PavolMilcik <pavolmilcik@gmail.com>
2023-07-10 13:12:50 +0200

    Add fibonacci funciton from to main program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.math import sum_number
 from utils.factorial import my_factorial
+from utils.fibonacci import fibo
 
 
 if __name__ == "__main__":
@@ -8,3 +9,7 @@ if __name__ == "__main__":
     print(sum_number(3, 4))
     
     print(my_factorial(5))
+
+    print("\n")
+    fibo()
+    print("\n")
