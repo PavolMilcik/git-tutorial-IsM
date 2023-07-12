a2473f9b621f677e88cb8ecfc7d5ef0c2d47ba60
PavolMilcik <pavolmilcik@gmail.com>
2023-07-12 15:46:19 +0200

    Add function which return prime numbers

---
+++ utils/prime_number.py
@@ -0,0 +1,13 @@
+def prime_nmbr(x):
+    count_prime_numbers = 0
+    for i in range(1, x+1):
+        count_modulos = 0
+        for j in range(1, i):
+            if i % j == 0:
+                count_modulos += 1
+            if count_modulos > 1:
+                break
+        if count_modulos <= 1:
+            count_prime_numbers += 1
+            print(i)
+    print("\nCount of prime numbers between 1 and " + str(x) + " is: " + str(count_prime_numbers) + "\n")
